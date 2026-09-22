b4599479b08d3e04ce743b7335d0f77ede83ca77
Clifford Usidamen <clifford.usidamen@edves.net>
2026-09-22 12:53:49 +0100

    Add helper function for formating date time

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -2,6 +2,7 @@ import type { InertiaLinkProps } from '@inertiajs/react';
 import { clsx } from 'clsx';
 import type { ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
+import { DateTime } from 'luxon';
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
@@ -9,4 +10,8 @@ export function cn(...inputs: ClassValue[]) {
 
 export function toUrl(url: NonNullable<InertiaLinkProps['href']>): string {
     return typeof url === 'string' ? url : url.url;
+}
+
+export function formatDate(dateString: string, format: string = 'yyyy-MM-dd HH:mm:ss'): string {
+    return DateTime.fromISO(dateString).toFormat(format);
 }
